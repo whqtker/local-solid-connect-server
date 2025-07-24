f528ac45f4dd28bb6ccfb9a7c0e6bef3d45e3a76
seonghyeok <chosh2001@naver.com>
2025-07-25 00:35:33 +0900

    1

--- src/main/java/com/example/solidconnection/auth/client/AppleOAuthClient.java
+++ src/main/java/com/example/solidconnection/auth/client/AppleOAuthClient.java
@@ -25,11 +25,14 @@ import org.springframework.web.client.RestTemplate;
  * 애플 인증을 위한 OAuth2 클라이언트
  * https://developer.apple.com/documentation/signinwithapplerestapi/generate_and_validate_tokens
  * */
+// 애플로부터 받은 인증 코드를 통해 사용자 정보가 담긴 토큰을 받고 이를 검증한 후 정보를 추출한다.
 @Component
 @RequiredArgsConstructor
 public class AppleOAuthClient {
 
+    // 외부 API와 통신을 하기 위한 스프링 도구
     private final RestTemplate restTemplate;
+
     private final AppleOAuthClientProperties properties;
     private final AppleOAuthClientSecretProvider clientSecretProvider;
     private final ApplePublicKeyProvider publicKeyProvider;
@@ -40,6 +43,7 @@ public class AppleOAuthClient {
         return new AppleUserInfoDto(parseEmailFromToken(applePublicKey, idToken));
     }
 
+    // 토큰 발급 엔드포인트로 HTTP 요청 보냄
     public String requestIdToken(String code) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
@@ -58,14 +62,22 @@ public class AppleOAuthClient {
         }
     }
 
+    // 토큰 요청에 필요한 파라미터 구성
     private MultiValueMap<String, String> buildFormData(String code) {
+        // MultiValueMap: key 하나에 여러 value, 개념적으로 Map<String, List<String>>와 동일함.
+        // LinkedMultiValueMap: MultiValueMap의 구현체로, 순서가 보장됨.
         MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+
         formData.add("client_id", properties.clientId());
         formData.add("client_secret", clientSecretProvider.generateClientSecret());
-        formData.add("code", code);
+        formData.add("code", code); // 로그인 후 애플로부터 받은 일회용 인증용 코드
         formData.add("grant_type", "authorization_code");
         formData.add("redirect_uri", properties.redirectUrl());
         return formData;
+
+        // Q. 하나의 key에 하나의 value가 매핑되고 있는데 왜 MultiValueMap을 사용하는가 ?
+        // A. RestTemplate과 데이터를 주고받기 위한 사실상 공식 자료구조임.
+        //    또한 HTTP 폼 데이터는 하나의 키에 여러 값을 가질 수 있음, 향후 변경에 유연하게 대처 가능
     }
 
     private String parseEmailFromToken(PublicKey applePublicKey, String idToken) {
--- src/main/java/com/example/solidconnection/auth/client/config/AppleOAuthClientProperties.java
+++ src/main/java/com/example/solidconnection/auth/client/config/AppleOAuthClientProperties.java
@@ -2,6 +2,7 @@ package com.example.solidconnection.auth.client.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+// application-xxx.yml에 정의된 속성을 자바 객체 필드로 자동으로 바인딩해줌.
 @ConfigurationProperties(prefix = "oauth.apple")
 public record AppleOAuthClientProperties(
         String tokenUrl,
